1a0e4f54205553989cad8942d33541fde28e852d
ioofy <mynev.id@gmail.com>
2023-02-12 02:15:23 -0500

    feat: adding key

--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -3,6 +3,8 @@ import { component$, Slot } from "@builder.io/qwik";
 import Footer from "~/components/widgets/Footer";
 import Header from "~/components/widgets/Header";
 
+// layout always be layout at the every page
+
 export default component$(() => {
   return (
     <>
